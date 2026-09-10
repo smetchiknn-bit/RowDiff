b2ca72e8acf211a8d9d182ac34fe48ae1757bfc7
SmetchikNN <smetchiknn@gmail.com>
2026-09-10 17:25:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import * as XLSX from "xlsx"
 // Замените эти значения на ваши собственные ключи
 // Инструкции по получению ключей см. ниже
 // ============================================
-const GOOGLE_API_KEY = "YOUR_API_KEY_HERE"
+const GOOGLE_API_KEY = "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"
 const GOOGLE_CLIENT_ID = "42845015067-gn44uo7hfg7gpsite001tl3ev78kb8rm.apps.googleusercontent.com"
 
 const FileIcon = () => <svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>
